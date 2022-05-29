c989932eeb7cd2c59905a20b0f49bb8824a090d8
Koronics Ádám <adam.koronics@gmail.com>
2022-05-29 19:12:13 +0200

    Fronted update

---
+++ frontend/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Test Page with Vue</h1>
+    <p>Just click on this button</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>Choose</h4>
+        <div class="flex justify-center gap-4">
+          <button class="inline-block px-6 py-2 rounded bg-blue-600 text-white font-medium">Want me</button>
+          <button class="inline-block px-6 py-2 rounded bg-blue-600 text-white font-medium">Really want me</button>
+          <button class="inline-block px-6 py-2 rounded bg-blue-600 text-white font-medium">Just kidding</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

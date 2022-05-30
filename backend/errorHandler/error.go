72a71c0620b7c179c0617837c138fd9c0e8b0d4b
Adamkoro <adam.koronics@gmail.com>
2022-05-30 18:15:31 +0200

    Removed ":" from errortypes

--- backend/errorHandler/error.go
+++ backend/errorHandler/error.go
@@ -6,9 +6,9 @@ import (
 )
 
 var (
-	WarningLogger = log.New(os.Stdout, "[WARNING]: ", log.Ldate|log.Ltime)
-	ErrorLogger   = log.New(os.Stderr, "[ERROR]: ", log.Ldate|log.Ltime)
-	InfoLogger    = log.New(os.Stdout, "[INFO]: ", log.Ldate|log.Ltime)
+	WarningLogger = log.New(os.Stdout, "[WARNING] ", log.Ldate|log.Ltime)
+	ErrorLogger   = log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime)
+	InfoLogger    = log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime)
 )
 
 func IsError(err error) bool {

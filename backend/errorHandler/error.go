a172e8f6e993c4be1345c48daece08dd10884083
Koronics Ádám <adam.koronics@gmail.com>
2022-05-30 16:48:57 +0200

    InfoLogger add

--- backend/errorHandler/error.go
+++ backend/errorHandler/error.go
@@ -6,8 +6,9 @@ import (
 )
 
 var (
-	WarningLogger = log.New(os.Stderr, "[WARNING]: ", log.Ldate|log.Ltime)
+	WarningLogger = log.New(os.Stdout, "[WARNING]: ", log.Ldate|log.Ltime)
 	ErrorLogger   = log.New(os.Stderr, "[ERROR]: ", log.Ldate|log.Ltime)
+	InfoLogger    = log.New(os.Stdout, "[INFO]: ", log.Ldate|log.Ltime)
 )
 
 func IsError(err error) bool {

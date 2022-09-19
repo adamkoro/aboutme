0d20d0572abe73545f129093d7b68c0d000d849e
adamkoro <akoronics@protonmail.com>
2022-09-19 19:47:25 +0200

    Server side fetch working

--- frontend/components/Home.vue
+++ frontend/components/Home.vue
@@ -2,9 +2,10 @@
     <Navbar />
     <div class="flex items-center justify-center bg-white dark:bg-black">
         <h1 class="text-black dark:text-white">Welcome to my page!</h1>
+        <p> {{ test }}</p>
     </div>
 </template>
 
-<script>
-
+<script setup>
+    const { data: test } = await useFetch("https://catfact.ninja/fact", { server: true, defer: true });
 </script>
